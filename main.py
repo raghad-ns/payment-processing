a3885ebb9057482b12c2f4d0b887be597d8872b2
Raghad Salem <raghadnadersalem20020217@gmail.com>
2024-08-17 21:15:43 +0300

    Add validation for each payment strategy

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-def cridit_card_payment (order):
-    return order.price * 1.02
+from abc import ABC,abstractmethod
+# Assume
 
-def paypal_payment (order):
-    return order.price + 5
+# Payment interface
+class Payment(ABC):
+    @abstractmethod
+    def process_payment(self, order):
+        pass
 
-def cryptocurrency_payment(order):
-    return order.price * 1.1
+# Validation interface
+class Validation(ABC):
+    @abstractmethod
+    def validate_payment(self):
+        pass
+
+class Cridit_card_payment(Payment, Validation):
+    def process_payment (self, order):
+        if (self.validate_payment()):
+            return order.price * 1.02
+    
+    def validate_payment(self):
+        # Assuming valid payment
+        return True
+
+class Paypal_payment(Payment, Validation):
+    def process_payment (self, order):
+        if (self.validate_payment()):
+            return order.price + 5
+    
+    def validate_payment(self):
+        # Assuming valid payment
+        return True
+
+class Cryptocurrency_payment(Payment, Validation):
+    def process_payment(self, order):
+        if (self.validate_payment()):
+            return order.price * 1.1
+    
+    def validate_payment(self):
+        # Assuming valid payment
+        return True
 
 class Order:
     def __init__(self, items, price, payment_method):
@@ -13,8 +46,8 @@ class Order:
         self.price = price
         self.items = items
     def process_payment(self):
-        self.total_price = self.payment_method(self)
+        self.total_price = self.payment_method.process_payment(self)
 
-order = Order(['Apple', 'Banana', 'Tomato'], 100, cryptocurrency_payment)
+order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment())
 order.process_payment()
 print(order.total_price)
