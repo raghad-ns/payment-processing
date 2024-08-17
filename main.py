2cd86c0e6f13b996d2432bdaa00b8e9019671b00
Raghad Salem <raghadnadersalem20020217@gmail.com>
2024-08-17 21:35:00 +0300

    Implement discounts strategies

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Validation(ABC):
 class Cridit_card_payment(Payment, Validation):
     def process_payment (self, order):
         if (self.validate_payment()):
-            return order.price * 1.02
+            return order.total_price * 1.02
     
     def validate_payment(self):
         # Assuming valid payment
@@ -25,7 +25,7 @@ class Cridit_card_payment(Payment, Validation):
 class Paypal_payment(Payment, Validation):
     def process_payment (self, order):
         if (self.validate_payment()):
-            return order.price + 5
+            return order.total_price + 5
     
     def validate_payment(self):
         # Assuming valid payment
@@ -34,20 +34,46 @@ class Paypal_payment(Payment, Validation):
 class Cryptocurrency_payment(Payment, Validation):
     def process_payment(self, order):
         if (self.validate_payment()):
-            return order.price * 1.1
+            return order.total_price * 1.1
     
     def validate_payment(self):
         # Assuming valid payment
         return True
 
+# Discount interface
+class Discount(ABC):
+    @abstractmethod
+    def calculate_discount(self, order):
+        pass
+
+class Percentage_based_discount(Discount):
+    def __init__(self, discount_percentage) :
+        self.discount_percentage = discount_percentage
+
+    def calculate_discount(self, order):
+        return order.price * self.discount_percentage
+
+class Fixed_amount_discount(Discount):
+    def __init__(self, discount_amount) :
+        self.discount_amount = discount_amount
+
+    def calculate_discount(self, order):
+        return self.discount_amount
 class Order:
-    def __init__(self, items, price, payment_method):
+    def __init__(self, items, price, payment_method, discount_strategy):
         self.payment_method = payment_method
+        self.discount_strategy = discount_strategy
         self.price = price
         self.items = items
+        self.total_price = price # initially it equals the initial price, without discounts or payment fees
+    
     def process_payment(self):
         self.total_price = self.payment_method.process_payment(self)
+    
+    def apply_discount(self):
+        self.total_price -= self.discount_strategy.calculate_discount(self)
 
-order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment())
+order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment(), Percentage_based_discount(0.5))
+order.apply_discount()
 order.process_payment()
 print(order.total_price)
