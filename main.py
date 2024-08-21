3fc2f412f49b43d6b3f8a24d1fab98fab0ab0a01
Raghad Salem <raghadnadersalem20020217@gmail.com>
2024-08-21 14:07:27 +0300

    Adding unit tests

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from abc import ABC,abstractmethod
-from Logger import Logger
-from discounts.discount_strategies.Percentage_based_discount import Percentage_based_discount
+import unittest
+from order.Order import Order
 from payment.payment_stratigies.Cryptocurrency_payment import Cryptocurrency_payment
-from payment.payment_stratigies.Paypal_payment import Paypal_payment 
+from discounts.discount_strategies.Percentage_based_discount import Percentage_based_discount
 
 
-class Order:
-    def __init__(self, items, price, payment_method, discount_strategy):
-        self.payment_method = payment_method
-        self.discount_strategy = discount_strategy
-        self.price = price
-        self.items = items
-        self.total_price = price # initially it equals the initial price, without discounts or payment fees
+class Test(unittest.TestCase):
+    def test_discount(self):
+        order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment(), Percentage_based_discount(0.5))
+        order.apply_discount()
+        self.assertEqual(int(order.total_price), 50)
     
-    def process_payment(self):
-        self.total_price = self.payment_method.process_payment(self)
-    
-    def apply_discount(self):
-        self.total_price -= self.discount_strategy.calculate_discount(self)
+    def test_payment(self):
+        order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment(), Percentage_based_discount(0.5))
+        order.process_payment()
+        self.assertEqual(int(order.total_price), 110)
 
-order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment(), Percentage_based_discount(0.5))
-order.apply_discount()
-order.process_payment()
-order = Order(['Apple', 'Banana', 'Tomato'], 100, Paypal_payment(), Percentage_based_discount(0.5))
-order.process_payment()
-print(order.total_price)
+    def test_payment_with_discount(self):
+        order = Order(['Apple', 'Banana', 'Tomato'], 100, Cryptocurrency_payment(), Percentage_based_discount(0.5))
+        order.apply_discount()
+        order.process_payment()
+        self.assertEqual(int(order.total_price), 55)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ order/Order.py
@@ -0,0 +1,14 @@
+
+class Order:
+    def __init__(self, items, price, payment_method, discount_strategy):
+        self.payment_method = payment_method
+        self.discount_strategy = discount_strategy
+        self.price = price
+        self.items = items
+        self.total_price = price # initially it equals the initial price, without discounts or payment fees
+    
+    def process_payment(self):
+        self.total_price = self.payment_method.process_payment(self)
+    
+    def apply_discount(self):
+        self.total_price -= self.discount_strategy.calculate_discount(self)
